e3b1dfff3043135ded83dd76caffd1e2f63323cc
sanchezign <sanchez.ign1995@gmail.com>
2025-12-10 14:30:46 -0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+site: "https://sanchezign.github.com/",
+vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+});
---
+++ src/data/projetsAboutData.js
@@ -0,0 +1,35 @@
+import urbanStyle from "../assets/img-projects/urbanStyle.png";
+import imgNote from "../assets/img-projects/note-app.png";
+
+export const proyectos = [
+  {
+    titulo: "E-commerce Urban Style",
+    descripcion: "Descripción breve del proyecto 1.",
+    imagen: urbanStyle.src,
+    tecnologias: ["React", "Tailwind", "Firebase"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+  },
+  {
+    titulo: "Tienda Artesana",
+    imagen:
+      "https://i.pinimg.com/736x/c9/70/bf/c970bfbc24489e39418b2c1b1f8f74ef.jpg",
+    tecnologias: ["React", "Node", "Express", "MongoDB"],
+    demo: "https://c21-05-ft-node-react-mirror-frontend-64qbwssi2-muyvisual.vercel.app/",
+    codigo: "https://github.com/No-Country-simulation/c21-05-ft-node-react",
+  },
+  {
+    titulo: "App organizacion",
+    imagen: imgNote.src,
+    tecnologias: ["React", "Tailwind", "Express", "MongoDB"],
+    demo: "https://front-react-todo.onrender.com",
+    codigo: "https://github.com/Lautaro-R-collins/Front-react-todo",
+  },
+  {
+    titulo: "API REST",
+    imagen: "https://miro.medium.com/1*47S4iWVwTicFkFL4-Z4uAA.png",
+    tecnologias: ["Express", "Node.js", "MongoDB", "Mongoose"],
+    demo: "https://back-express-todo.onrender.com",
+    codigo: "https://github.com/Lautaro-R-collins/Back-express-todo.git",
+  },
+];
